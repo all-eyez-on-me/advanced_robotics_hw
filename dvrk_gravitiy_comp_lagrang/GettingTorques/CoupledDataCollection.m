b6b4d7dbb2307a5397e566a6d4c9ae68b4d9eac8
linhongbin <522181941@qq.com>
2018-03-12 15:15:34 +0800

    add two data collector

--- dvrk_gravitiy_comp_lagrang/GettingTorques/CoupledDataCollection.m
+++ dvrk_gravitiy_comp_lagrang/GettingTorques/CoupledDataCollection.m
@@ -1,30 +1,24 @@
 %Home();
 %pause(10);
 %rosinit
-clc; clearvars;
+clear all;clc
 rosshutdown
 rosinit
 n = 15 ;
 waiting_time = 5;
-sub_tor = rossubscriber('/dvrk/MTMR/joint_states');
-sub_pos = rossubscriber('/dvrk/MTMR/joint_states');
-pub_pos = rospublisher('/dvrk/MTMR/desired_state', 'sensor_msgs/JointState');
+sub_pos = rossubscriber('/dvrk/MTMR/state_joint_current');
+sub_tor = rossubscriber('/dvrk/MTMR/state_joint_current');
+pub_pos = rospublisher('/dvrk/MTMR/set_position_joint');
 %Set_Position(pub_pos, qs(:,i));
 saved_data = zeros(7,2,n);
 data = zeros(7,2,10);
 q_min = [-0.6981,-0.2618, -0.8727, -3.4907, -1.5708 ,-0.7854,-8.3776];
 q_max = [1.1345, 0.8727 , 0.6109 ,  1.5708 , 3.1416, 0.7854,7.8540];
 qs = rand(7,n);
-% for i = 1:15
-%        qs(1:3,i) = positions(1:3,2);
-% end
-% 
-% for i = 16:30
-%        qs(1:3,i) = positions(1:3,3);
-% end
+
 
 for i = 1:n
-    qs(:,i) = qs(:,i).*(q_max'-q_min')*.8 + q_min';
+    qs(:,i) = qs(:,i).*(q_max'-q_min')*.6 + q_min';
     qs(1,i) = 0;
     qs(6:7,i) = [0,0].'
 end
@@ -47,4 +41,11 @@ for i = 1:n
     sprintf('The %d set of Data is save',i)
 end
 
-save('save_sim_data', 'saved_data')
+if exist('Couple Data')~=7
+    mkdir('Couple Data');
+end
+data_dir = dir('Couple Data');
+data_dir_name = data_dir;
+file_num = length(data_dir_name)
+file_name=sprintf('Couple Data/%d.mat',file_num-1);
+save(file_name); 
--- dvrk_gravitiy_comp_lagrang/Gravity Compensation Final/GravityCompensationModified_PID.m
+++ dvrk_gravitiy_comp_lagrang/Gravity Compensation Final/GravityCompensationModified_PID.m
@@ -2,6 +2,7 @@ rosshutdown
 rosinit
 sub_pos = rossubscriber('/dvrk/MTMR/joint_states');
 sub_tor = rossubscriber('dvrk/MTMR/joint_states');
+sub_vel = rossubscriber('dvrk/MTMR/joint_states');
 pub_tor = rospublisher('/dvrk/MTMR/set_effort_joint');
  g = 9.81;
  r = rosrate(150); % increase ros refresh rate to 150 Hz
@@ -37,13 +38,30 @@ pub_tor = rospublisher('/dvrk/MTMR/set_effort_joint');
    -0.0000
 ]
 
+q_des = [0;0;0;0;0;0;0];
+past_q_past = zeros(7,1);
+error_sum = zeros(7,1);
 
+
+P = -1*[0.5,0.4,3.7,0.5,0.2,0.006,0.0001]
+I = -1*[0.00001,0.00005,0.0001,0.0004,0.000001,0.0000001,0.00000001]
+D = -1*[1.5,0.95,1.75,0.2,0.1,0.8,0.08]
+
+P_mat = diag(P);
+I_mat = diag(I);
+D_mat = diag(D);
+
+figure(1);
+title('Error between computed gravity torque and current acutal torque')
+legend('T1_error','T2_error','T3_error','T4_error','T5_error','T6_error','T7_error');
+error_plot = zeros(7,1)
 
 i = 0;
 
 while i ~= 1
 % for i = 1:100
   qs = Get_Position(sub_pos);
+  vel = Get_Velocity(sub_vel);
   q1 = qs(1);
   q2 = qs(2);
   q3 = qs(3);
@@ -54,6 +72,7 @@ while i ~= 1
   effort = Get_Torque(sub_tor);
   effort = effort(1:7);
   
+  
   Regressor_Matrix =             [         0,          0,                                     0,                                     0,                                                                                                                             0,                                                                                                                                                                                                                                     0,                                                     0,                                                     0,                                                                                                                             0,                                                                                                                                                                                                                                     0,                                                                                                                                                                                                                                                                                                                                                                                                                                                     0,                                                                                                                                                                                                                                                                                                                                                                                                                                                     0
                                    g*sin(q2), -g*cos(q2), g*cos(q2)*cos(q3) - g*sin(q2)*sin(q3), g*cos(q2)*sin(q3) + g*cos(q3)*sin(q2), g*cos(q4)*sin(q2)*sin(q3)*sin(q5) - g*cos(q3)*cos(q5)*sin(q2) - g*cos(q2)*cos(q3)*cos(q4)*sin(q5) - g*cos(q2)*cos(q5)*sin(q3), g*sin(q2)*sin(q3)*sin(q4)*sin(q6) - g*cos(q2)*cos(q6)*sin(q3)*sin(q5) - g*cos(q3)*cos(q6)*sin(q2)*sin(q5) - g*cos(q2)*cos(q3)*sin(q4)*sin(q6) - g*cos(q4)*cos(q5)*cos(q6)*sin(q2)*sin(q3) + g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*cos(q6), g*cos(q2)*cos(q3)*cos(q4) - g*cos(q4)*sin(q2)*sin(q3), g*cos(q2)*cos(q3)*sin(q4) - g*sin(q2)*sin(q3)*sin(q4), g*cos(q2)*sin(q3)*sin(q5) + g*cos(q3)*sin(q2)*sin(q5) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3), g*cos(q6)*sin(q2)*sin(q3)*sin(q4) - g*cos(q2)*cos(q3)*cos(q6)*sin(q4) + g*cos(q2)*sin(q3)*sin(q5)*sin(q6) + g*cos(q3)*sin(q2)*sin(q5)*sin(q6) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3)*sin(q6) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*sin(q6), g*cos(q2)*cos(q5)*sin(q3)*sin(q7) + g*cos(q3)*cos(q5)*sin(q2)*sin(q7) + g*cos(q2)*cos(q3)*cos(q4)*sin(q5)*sin(q7) + g*cos(q6)*cos(q7)*sin(q2)*sin(q3)*sin(q4) + g*cos(q2)*cos(q7)*sin(q3)*sin(q5)*sin(q6) + g*cos(q3)*cos(q7)*sin(q2)*sin(q5)*sin(q6) - g*cos(q4)*sin(q2)*sin(q3)*sin(q5)*sin(q7) - g*cos(q2)*cos(q3)*cos(q6)*cos(q7)*sin(q4) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*cos(q7)*sin(q6) + g*cos(q4)*cos(q5)*cos(q7)*sin(q2)*sin(q3)*sin(q6), g*cos(q4)*cos(q7)*sin(q2)*sin(q3)*sin(q5) - g*cos(q3)*cos(q5)*cos(q7)*sin(q2) - g*cos(q2)*cos(q3)*cos(q6)*sin(q4)*sin(q7) - g*cos(q2)*cos(q5)*cos(q7)*sin(q3) + g*cos(q6)*sin(q2)*sin(q3)*sin(q4)*sin(q7) + g*cos(q2)*sin(q3)*sin(q5)*sin(q6)*sin(q7) + g*cos(q3)*sin(q2)*sin(q5)*sin(q6)*sin(q7) - g*cos(q2)*cos(q3)*cos(q4)*cos(q7)*sin(q5) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3)*sin(q6)*sin(q7) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*sin(q6)*sin(q7)
                                            0,          0, g*cos(q2)*cos(q3) - g*sin(q2)*sin(q3), g*cos(q2)*sin(q3) + g*cos(q3)*sin(q2), g*cos(q4)*sin(q2)*sin(q3)*sin(q5) - g*cos(q3)*cos(q5)*sin(q2) - g*cos(q2)*cos(q3)*cos(q4)*sin(q5) - g*cos(q2)*cos(q5)*sin(q3), g*sin(q2)*sin(q3)*sin(q4)*sin(q6) - g*cos(q2)*cos(q6)*sin(q3)*sin(q5) - g*cos(q3)*cos(q6)*sin(q2)*sin(q5) - g*cos(q2)*cos(q3)*sin(q4)*sin(q6) - g*cos(q4)*cos(q5)*cos(q6)*sin(q2)*sin(q3) + g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*cos(q6), g*cos(q2)*cos(q3)*cos(q4) - g*cos(q4)*sin(q2)*sin(q3), g*cos(q2)*cos(q3)*sin(q4) - g*sin(q2)*sin(q3)*sin(q4), g*cos(q2)*sin(q3)*sin(q5) + g*cos(q3)*sin(q2)*sin(q5) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3), g*cos(q6)*sin(q2)*sin(q3)*sin(q4) - g*cos(q2)*cos(q3)*cos(q6)*sin(q4) + g*cos(q2)*sin(q3)*sin(q5)*sin(q6) + g*cos(q3)*sin(q2)*sin(q5)*sin(q6) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3)*sin(q6) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*sin(q6), g*cos(q2)*cos(q5)*sin(q3)*sin(q7) + g*cos(q3)*cos(q5)*sin(q2)*sin(q7) + g*cos(q2)*cos(q3)*cos(q4)*sin(q5)*sin(q7) + g*cos(q6)*cos(q7)*sin(q2)*sin(q3)*sin(q4) + g*cos(q2)*cos(q7)*sin(q3)*sin(q5)*sin(q6) + g*cos(q3)*cos(q7)*sin(q2)*sin(q5)*sin(q6) - g*cos(q4)*sin(q2)*sin(q3)*sin(q5)*sin(q7) - g*cos(q2)*cos(q3)*cos(q6)*cos(q7)*sin(q4) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*cos(q7)*sin(q6) + g*cos(q4)*cos(q5)*cos(q7)*sin(q2)*sin(q3)*sin(q6), g*cos(q4)*cos(q7)*sin(q2)*sin(q3)*sin(q5) - g*cos(q3)*cos(q5)*cos(q7)*sin(q2) - g*cos(q2)*cos(q3)*cos(q6)*sin(q4)*sin(q7) - g*cos(q2)*cos(q5)*cos(q7)*sin(q3) + g*cos(q6)*sin(q2)*sin(q3)*sin(q4)*sin(q7) + g*cos(q2)*sin(q3)*sin(q5)*sin(q6)*sin(q7) + g*cos(q3)*sin(q2)*sin(q5)*sin(q6)*sin(q7) - g*cos(q2)*cos(q3)*cos(q4)*cos(q7)*sin(q5) + g*cos(q4)*cos(q5)*sin(q2)*sin(q3)*sin(q6)*sin(q7) - g*cos(q2)*cos(q3)*cos(q4)*cos(q5)*sin(q6)*sin(q7)
@@ -63,20 +82,39 @@ while i ~= 1
                                            0,          0,                                     0,                                     0,                                                                                                                             0,                                                                                                                                                                                                                                     0,                                                     0,                                                     0,                                                                                                                             0,                                                                                                                                                                                                                                     0,                                                                                                                                                                                                                                                                                                                                                                                                                                                     0,                                                                                                                                                                                                                                                                                                                                                                                                                                                    0]; %g*(cos(q5)*cos(q7)*sin(q2)*sin(q3) - cos(q2)*cos(q3)*cos(q5)*cos(q7) + cos(q2)*cos(q4)*cos(q7)*sin(q3)*sin(q5) + cos(q3)*cos(q4)*cos(q7)*sin(q2)*sin(q5) + cos(q2)*cos(q6)*sin(q3)*sin(q4)*sin(q7) + cos(q3)*cos(q6)*sin(q2)*sin(q4)*sin(q7) + cos(q2)*cos(q3)*sin(q5)*sin(q6)*sin(q7) - sin(q2)*sin(q3)*sin(q5)*sin(q6)*sin(q7) + cos(q2)*cos(q4)*cos(q5)*sin(q3)*sin(q6)*sin(q7) + cos(q3)*cos(q4)*cos(q5)*sin(q2)*sin(q6)*sin(q7)),                -g*(cos(q2)*cos(q3)*cos(q5)*sin(q7) - cos(q5)*sin(q2)*sin(q3)*sin(q7) + cos(q2)*cos(q6)*cos(q7)*sin(q3)*sin(q4) + cos(q3)*cos(q6)*cos(q7)*sin(q2)*sin(q4) + cos(q2)*cos(q3)*cos(q7)*sin(q5)*sin(q6) - cos(q2)*cos(q4)*sin(q3)*sin(q5)*sin(q7) - cos(q3)*cos(q4)*sin(q2)*sin(q5)*sin(q7) - cos(q7)*sin(q2)*sin(q3)*sin(q5)*sin(q6) + cos(q2)*cos(q4)*cos(q5)*cos(q7)*sin(q3)*sin(q6) + cos(q3)*cos(q4)*cos(q5)*cos(q7)*sin(q2)*sin(q6))];
 
 
-  Torques = Regressor_Matrix*dynamic_param;
+  G_Torques = Regressor_Matrix*dynamic_param;
+  
+
 %   Torques = [Torques(1);Torques(2);Torques(3);...
 %       Torques(4);Torques(5);Torques(6);Torques(7)];
-  Torques = [0;Torques(2);Torques(3);...
-      Torques(4);Torques(5);Torques(6);0];
+  G_Torques = [0;G_Torques(2);G_Torques(3);...
+      G_Torques(4);G_Torques(5);G_Torques(6);0];
   %Torques(6:7) = zeros(1, 2);
+  
+    q_error = qs - q_des;
+    error_sum = error_sum + q_error;
+  P_Torques = P_mat * q_error;
+  D_Torques = D_mat * vel;
+  I_Torques = I_mat * error_sum;
+  
+  
+  Torques = P_Torques + I_Torques + D_Torques + G_Torques;
   Set_Torque(pub_tor, Torques);
 %   pause(0.01);
 %   position_error(1:8,i) = Get_Position(sub_pos)-position;
-  display('running')
+  %display('running')
 %   torque_table(:,i)=Torques(:);
 %   effort_table(:,i)=effort(:);
 %   pos_table(:,i)=[q1;q2;q3;q4;q5;q6;q7];
-  compare_torque = [effort';Torques']
+  compare_torque = [effort G_Torques];
+  error_plot = [error_plot,effort - G_Torques];
+  for k = 1:7
+      plot(error_plot(k,:));
+      hold on;
+  end
+  hold off;
+  title('Error between computed gravity torque and current acutal torque')
+  legend('T1 Error','T2 Error','T3 Error','T4 Error','T5 Error','T6 Error','T7 Error');
   loop = loop+1;
   display(loop)
   time = r.TotalElapsedTime;
